1ebba0bd65b3c11e22a147034bc7316fdc5b8925
juchan <rlawncks125@naver.com>
2024-02-21 08:51:20 +0900

    ..

--- src/app/naver/page.tsx
+++ src/app/naver/page.tsx
@@ -36,30 +36,14 @@ export default function page() {
           />
         </div>
         <button onClick={onRefreshToken}>refresh</button>
-        {userInfo && (
-          <div>
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th className="px-6 py-3">속성</th>
-                  <th className="px-6 py-3">값</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.keys(userInfo).map((key) => (
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <td className="px-6 py-3" key={key}>
-                      {key}
-                    </td>
-                    <td className="px-6 py-3">
-                      {/* @ts-ignore */}
-                      {userInfo[key]}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+        {user && (
+          <UserCard
+            className="bg-[##03C75A]"
+            email={user.email}
+            imageURL={user.profile_image}
+            name={user.name}
+            id={user.id}
+          />
         )}
       </main>
     </>
